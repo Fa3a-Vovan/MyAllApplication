bc3c777d47372532597dfcdccc9b834018cb14fd
Fa3a-Vovan <onizukaonibaku17@gmail.com>
2021-03-26 00:20:02 +0200

    activityUpdate

--- lesson12Activity_AS/src/main/java/com/example/lesson12activity_as/OneActivity.java
+++ lesson12Activity_AS/src/main/java/com/example/lesson12activity_as/OneActivity.java
@@ -1,34 +1,30 @@
 package com.example.lesson12activity_as;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OneActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "myLogs";
     TextView textView;
     EditText editText;
     Button button, button2;
     int k = 0;
-int k1 = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d(TAG, "MainActivity onCreate");
-
-        Log.d(TAG, "Main k="+k);
+        setContentView(R.layout.one_activity);
+        Log.d(TAG, "One k="+k);
         Intent intent = getIntent();
         k=intent.getIntExtra("k", k);
-        Log.d(TAG, "Main: k="+k);
+        Log.d(TAG, "One k="+k);
 
         textView = findViewById(R.id.text);
         editText = findViewById(R.id.editText);
@@ -36,60 +32,43 @@ int k1 = 0;
         button2 = findViewById(R.id.button2);
         button.setOnClickListener(this);
         button2.setOnClickListener(this);
-        textView.setText("Main"+k);
-
-
+        textView.setText("One"+k);
     }
 
     @Override
     protected void onStart() {
-        Log.d(TAG, "MainActivity onStart");
+        Log.d(TAG, "OneActivity onStart");
         super.onStart();
     }
 
     @Override
     protected void onResume() {
-        Log.d(TAG, "MainActivity onResume");
+        Log.d(TAG, "OneActivity onResume");
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        Log.d(TAG, "MainActivity onPause");
+        Log.d(TAG, "OneActivity onPause");
         super.onPause();
     }
 
     @Override
     protected void onStop() {
-        Log.d(TAG, "MainActivity onStop");
+        Log.d(TAG, "OneActivity onStop");
         super.onStop();
     }
 
     @Override
     protected void onRestart() {
-        Log.d(TAG, "MainActivity onRestart");
+        Log.d(TAG, "OneActivity onRestart");
         super.onRestart();
     }
 
     @Override
     protected void onDestroy() {
-        Log.d(TAG, "MainActivity Destroy");
+        Log.d(TAG, "OneActivity Destroy");
         super.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        Log.d(TAG, "onSaveInstanceState");
-        super.onSaveInstanceState(outState);
-        outState.putInt("k1", k1);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        Log.d(TAG, "onRestoreInstanceState");
-        super.onRestoreInstanceState(savedInstanceState);
-        k1 = savedInstanceState.getInt("k1");
-        textView.setText("Главн "+k1); k1++;
     }
 
     @Override
@@ -97,9 +76,9 @@ int k1 = 0;
         k++;
         Intent intent = null;
         textView.setText(editText.getText()+""+k);
-        switch (v.getId()) {
-            case R.id.button: intent = new Intent(this, OneActivity.class); break;
-            case R.id.button2: intent = new Intent(this, TwoActivity.class); break;
+        switch (v.getId()){
+            case R.id.button: intent = new Intent(this, TwoActivity.class); break;
+            case R.id.button2: intent = new Intent(this, MainActivity.class); break;
         }
         intent.putExtra("k", k);
         startActivity(intent);
--- lesson12Activity_AS/src/main/java/com/example/lesson12activity_as/TwoActivity.java
+++ lesson12Activity_AS/src/main/java/com/example/lesson12activity_as/TwoActivity.java
@@ -1,34 +1,31 @@
 package com.example.lesson12activity_as;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TwoActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "myLogs";
     TextView textView;
     EditText editText;
     Button button, button2;
     int k = 0;
-int k1 = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d(TAG, "MainActivity onCreate");
+        setContentView(R.layout.two_activity);
 
-        Log.d(TAG, "Main k="+k);
+        Log.d(TAG, "Two k="+k);
         Intent intent = getIntent();
         k=intent.getIntExtra("k", k);
-        Log.d(TAG, "Main: k="+k);
+        Log.d(TAG, "Two k="+k);
 
         textView = findViewById(R.id.text);
         editText = findViewById(R.id.editText);
@@ -36,60 +33,44 @@ int k1 = 0;
         button2 = findViewById(R.id.button2);
         button.setOnClickListener(this);
         button2.setOnClickListener(this);
-        textView.setText("Main"+k);
-
+        textView.setText("Two"+k);
 
     }
 
     @Override
     protected void onStart() {
-        Log.d(TAG, "MainActivity onStart");
+        Log.d(TAG, "TwoActivity onStart");
         super.onStart();
     }
 
     @Override
     protected void onResume() {
-        Log.d(TAG, "MainActivity onResume");
+        Log.d(TAG, "TwoActivity onResume");
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        Log.d(TAG, "MainActivity onPause");
+        Log.d(TAG, "TwoActivity onPause");
         super.onPause();
     }
 
     @Override
     protected void onStop() {
-        Log.d(TAG, "MainActivity onStop");
+        Log.d(TAG, "TwoActivity onStop");
         super.onStop();
     }
 
     @Override
     protected void onRestart() {
-        Log.d(TAG, "MainActivity onRestart");
+        Log.d(TAG, "TwoActivity onRestart");
         super.onRestart();
     }
 
     @Override
     protected void onDestroy() {
-        Log.d(TAG, "MainActivity Destroy");
+        Log.d(TAG, "TwoActivity Destroy");
         super.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        Log.d(TAG, "onSaveInstanceState");
-        super.onSaveInstanceState(outState);
-        outState.putInt("k1", k1);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        Log.d(TAG, "onRestoreInstanceState");
-        super.onRestoreInstanceState(savedInstanceState);
-        k1 = savedInstanceState.getInt("k1");
-        textView.setText("Главн "+k1); k1++;
     }
 
     @Override
@@ -97,9 +78,9 @@ int k1 = 0;
         k++;
         Intent intent = null;
         textView.setText(editText.getText()+""+k);
-        switch (v.getId()) {
-            case R.id.button: intent = new Intent(this, OneActivity.class); break;
-            case R.id.button2: intent = new Intent(this, TwoActivity.class); break;
+        switch (v.getId()){
+            case R.id.button: intent = new Intent(this, MainActivity.class); break;
+            case R.id.button2: intent = new Intent(this, OneActivity.class); break;
         }
         intent.putExtra("k", k);
         startActivity(intent);
